394f211560fb7b6e267cb17ced379b6b68dfa201
zhou-yg <zhou.y.g890@gmail.com>
2015-03-01 23:18:50 +0800

    edit

--- Public/coffees/views/battle/initial.coffee
+++ Public/coffees/views/battle/initial.coffee
@@ -1,6 +1,8 @@
 ce = React.createElement
 cc = React.createClass
 
+screenWidth = window.screen.width
+
 HeroPanelClass = cc {
   render:->
     ce 'div',{ className:'hero' },null
@@ -39,37 +41,144 @@ PersonalBoxClass = cc {
 }
 
 ChessListClass = cc {
+  getInitialState:->
+    sWidth = screenWidth*0.96
+    perLeft = sWidth * 0.2
+    chessMap = @props.chessMap
+    chessMap.chessList = chessMap.chessList.map (chessOne,i)->
+      if typeof chessOne isnt 'object'
+        chessOne = {}
+      chessOne.key = 'chessLi'+i
+      chessOne.ox = perLeft*i
+      chessOne.oy = 10
+      chessOne.x = chessOne.ox + 'px'
+      chessOne.y = chessOne.oy + 'px'
+      return chessOne
+    {
+      chessMap:chessMap
+      touchesXY:{
+        x:0
+        y:0
+      }
+    }
+  touchOnChess:(ev)->
+    firstTouch = ev.nativeEvent.touches[0]
+    ev.target.style.zIndex = 10
+
+    @setState {
+      touchesXY:
+        x:firstTouch.clientX
+        y:firstTouch.clientY
+    }
+  moveOnChess:(ev)->
+    target = ev.target
+    key    = ev.dispatchMarker.match /[\w]*$/
+    key    = key[0]
+
+    chessMap = @state.chessMap
+
+    startXY = @state.touchesXY
+
+    firstTouch = ev.nativeEvent.touches[0]
+    firstTouch = {
+      x:firstTouch.clientX
+      y:firstTouch.clientY
+    }
+    for chessOne,i in chessMap.chessList
+      if chessOne.key is key
+        current = chessOne
+        break
+
+    current.x = current.ox + (firstTouch.x - startXY.x ) + 'px'
+    current.y = current.oy + (firstTouch.y - startXY.y ) + 'px'
+
+    target.style.left = current.x
+    target.style.top  = current.y
+
+  leaveChess:->
+    console.log 'touchend'
+
   render:->
-    listMap = @props.listMap
+    chessMap = @props.chessMap
+    that = this
     ce 'section',{ className:'behind-cards' },
-      (ce 'h3',{}, listMap.name )
+      (ce 'h3',{}, chessMap.name )
       (ce 'hr',{ className:'h-line'},null )
       (ce 'ul',{ className:'cards' },
-        listMap.chessList.map (chess,i)->
+        chessMap.chessList.map (chess,i)->
           bgImg = chess.img
           if bgImg
             bgImg = chessFactory.chessAvatarPre + bgImg
             return ce 'li',{
+              onTouchStart:that.touchOnChess
+              onTouchMove:that.moveOnChess
+              onTouchEnd:that.leaveChess
               style:{
                 backgroundImage:bgImg
+                top:chess.y
+                left:chess.x
               }
-              key:'chessLi'+i
+              key:chess.key
             }
           else
-            return ce 'li',{key:'chessLi'+i},null
+            return ce 'li',{
+              onTouchStart:that.touchOnChess
+              onTouchMove:that.moveOnChess
+              onTouchEnd:that.leaveChess
+              style:{
+                top:chess.y
+                left:chess.x
+              }
+              key:chess.key
+            },null
+      )
+}
+
+BottomOpBarClass = cc {
+  getInitialState:->
+    {}
+  render:->
+    ce 'ul',{ className:'start-battle' },
+      (ce 'li',{ className:'left' },
+        (ce 'a',{href:'#list'},'排行榜')
+      )
+      (ce 'li',{ className:'left' },
+        (ce 'a',{href:'#'},'无')
+      )
+      (ce 'li',{ className:'center' },
+        (ce 'a',{href:'#match'},'匹配')
+      )
+      (ce 'li',{ className:'right' },
+        (ce 'a',{href:'#edit'},'编辑')
+      )
+      (ce 'li',{ className:'right' },
+        (ce 'a',{href:'#setting'},'设置')
       )
 }
 
 PrepareUiClass = cc {
   render:->
     ce 'div',{},
-      (  ce PersonalBoxClass )
-      (ce ChessListClass,{ listMap:{ name:'表1',chessList:[1,2,3] } } )
-      (ce ChessListClass,{ listMap:{ name:'表2',chessList:[1,2,3] } } )
+      (ce PersonalBoxClass )
+      (ce ChessListClass,{ chessMap:{ name:'表1',chessList:[1,2,3] } } )
+      (ce ChessListClass,{ chessMap:{ name:'表2',chessList:[1,2,3] } } )
+      (ce BottomOpBarClass,{} )
+}
+
+BottomOpRoute = Backbone.Route.extend {
+  routes:{
+    'list':'list'
+    'match':'match'
+    'edit':'edit'
+    'setting':'setting'
+  }
+  list:->
+  match:->
+  edit:->
+  setting:->
 }
 
 prepareUiDom = document.querySelector '.prepare-ui'
-
 React.render(
   ce PrepareUiClass
   prepareUiDom
--- Public/js/views/battle/initial.js
+++ Public/js/views/battle/initial.js
@@ -1,9 +1,11 @@
 (function() {
-  var ChessListClass, HeroPanelClass, PersonalBoxClass, PrepareUiClass, RecordPanelClass, cc, ce, prepareUiDom;
+  var BottomOpBarClass, ChessListClass, HeroPanelClass, PersonalBoxClass, PrepareUiClass, RecordPanelClass, cc, ce, prepareUiDom, screenWidth;
 
   ce = React.createElement;
 
   cc = React.createClass;
+
+  screenWidth = window.screen.width;
 
   HeroPanelClass = cc({
     render: function() {
@@ -54,48 +56,155 @@
   });
 
   ChessListClass = cc({
+    getInitialState: function() {
+      var chessMap, perLeft, sWidth;
+      sWidth = screenWidth * 0.96;
+      perLeft = sWidth * 0.2;
+      chessMap = this.props.chessMap;
+      chessMap.chessList = chessMap.chessList.map(function(chessOne, i) {
+        if (typeof chessOne !== 'object') {
+          chessOne = {};
+        }
+        chessOne.key = 'chessLi' + i;
+        chessOne.ox = perLeft * i;
+        chessOne.oy = 10;
+        chessOne.x = chessOne.ox + 'px';
+        chessOne.y = chessOne.oy + 'px';
+        return chessOne;
+      });
+      return {
+        chessMap: chessMap,
+        touchesXY: {
+          x: 0,
+          y: 0
+        }
+      };
+    },
+    touchOnChess: function(ev) {
+      var firstTouch;
+      firstTouch = ev.nativeEvent.touches[0];
+      ev.target.style.zIndex = 10;
+      return this.setState({
+        touchesXY: {
+          x: firstTouch.clientX,
+          y: firstTouch.clientY
+        }
+      });
+    },
+    moveOnChess: function(ev) {
+      var chessMap, chessOne, current, firstTouch, i, key, startXY, target, _i, _len, _ref;
+      target = ev.target;
+      key = ev.dispatchMarker.match(/[\w]*$/);
+      key = key[0];
+      chessMap = this.state.chessMap;
+      startXY = this.state.touchesXY;
+      firstTouch = ev.nativeEvent.touches[0];
+      firstTouch = {
+        x: firstTouch.clientX,
+        y: firstTouch.clientY
+      };
+      _ref = chessMap.chessList;
+      for (i = _i = 0, _len = _ref.length; _i < _len; i = ++_i) {
+        chessOne = _ref[i];
+        if (chessOne.key === key) {
+          current = chessOne;
+          break;
+        }
+      }
+      current.x = current.ox + (firstTouch.x - startXY.x) + 'px';
+      current.y = current.oy + (firstTouch.y - startXY.y) + 'px';
+      target.style.left = current.x;
+      return target.style.top = current.y;
+    },
+    leaveChess: function() {
+      return console.log('touchend');
+    },
     render: function() {
-      var listMap;
-      listMap = this.props.listMap;
+      var chessMap, that;
+      chessMap = this.props.chessMap;
+      that = this;
       return ce('section', {
         className: 'behind-cards'
-      }, ce('h3', {}, listMap.name), ce('hr', {
+      }, ce('h3', {}, chessMap.name), ce('hr', {
         className: 'h-line'
       }, null), ce('ul', {
         className: 'cards'
-      }, listMap.chessList.map(function(chess, i) {
+      }, chessMap.chessList.map(function(chess, i) {
         var bgImg;
         bgImg = chess.img;
         if (bgImg) {
           bgImg = chessFactory.chessAvatarPre + bgImg;
           return ce('li', {
+            onTouchStart: that.touchOnChess,
+            onTouchMove: that.moveOnChess,
+            onTouchEnd: that.leaveChess,
             style: {
-              backgroundImage: bgImg
+              backgroundImage: bgImg,
+              top: chess.y,
+              left: chess.x
             },
-            key: 'chessLi' + i
+            key: chess.key
           });
         } else {
           return ce('li', {
-            key: 'chessLi' + i
+            onTouchStart: that.touchOnChess,
+            onTouchMove: that.moveOnChess,
+            onTouchEnd: that.leaveChess,
+            style: {
+              top: chess.y,
+              left: chess.x
+            },
+            key: chess.key
           }, null);
         }
       })));
     }
   });
 
+  BottomOpBarClass = cc({
+    getInitialState: function() {
+      return {};
+    },
+    render: function() {
+      return ce('ul', {
+        className: 'start-battle'
+      }, ce('li', {
+        className: 'left'
+      }, ce('a', {
+        href: '#'
+      }, '排行榜')), ce('li', {
+        className: 'left'
+      }, ce('a', {
+        href: '#'
+      }, '无')), ce('li', {
+        className: 'center'
+      }, ce('a', {
+        href: '#'
+      }, '匹配')), ce('li', {
+        className: 'right'
+      }, ce('a', {
+        href: '#'
+      }, '编辑')), ce('li', {
+        className: 'right'
+      }, ce('a', {
+        href: '#'
+      }, '设置')));
+    }
+  });
+
   PrepareUiClass = cc({
     render: function() {
       return ce('div', {}, ce(PersonalBoxClass), ce(ChessListClass, {
-        listMap: {
+        chessMap: {
           name: '表1',
           chessList: [1, 2, 3]
         }
       }), ce(ChessListClass, {
-        listMap: {
+        chessMap: {
           name: '表2',
           chessList: [1, 2, 3]
         }
-      }));
+      }), ce(BottomOpBarClass, {}));
     }
   });
 
